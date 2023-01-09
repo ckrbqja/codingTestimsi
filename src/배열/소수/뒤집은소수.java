859f71751225dfe9d31fd10582735ea18f66d198
ckrbqja <ckrbqja@gmail.com>
2023-01-09 18:37:08 +0900

    소수 루트

--- src/배열/소수/뒤집은소수.java
+++ src/배열/소수/뒤집은소수.java
@@ -21,9 +21,9 @@ public class 뒤집은소수 {
     }
 
     public static boolean isPrime(int num) {
-        if(num == 1) return false;
-        for (int i = 2; i < num ; i++) {
-            if(num % i == 0) return false;
+
+        for (int i = 2; i * i  < num; i++) {
+            if (num % i == 0) return false;
         }
         return true;
     }
